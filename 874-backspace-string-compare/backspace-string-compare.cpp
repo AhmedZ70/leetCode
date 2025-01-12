11d60d395c56c850147519242f285873ac2532e8
AhmedZ70 <101619413+AhmedZ70@users.noreply.github.com>
2025-01-12 12:38:10 -0700

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.14%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,33 +1,43 @@
 class Solution {
-    string stackToString(stack<char>& stk){
-        string s; 
-        while (!stk.empty()){
-            s += stk.top(); 
-            stk.pop(); 
-        }
-        reverse(s.begin(), s.end());
-        return s;
-    }
-    void addCharsToStack (stack<char>& stk, string s){
-        for (char c : s){
-            if (c != '#'){
-                stk.push(c);
-            }
-            else{
-                if (!stk.empty()){
-                    stk.pop();
-                }
-            }
-        }
-    }
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> stk; 
-        addCharsToStack(stk, s); 
-        s = stackToString(stk);
-        addCharsToStack(stk, t); 
-        t = stackToString(stk);
-
-        return (s == t);
+        // start at the end of each string 
+        int i = s.size() - 1;
+        int j = t.size() - 1; 
+        int skipS = 0, skipT = 0;
+        while (i >= 0 || j >= 0){
+            while (i >= 0 && (s[i] == '#' || skipS > 0)){
+                if (s[i] == '#'){
+                    skipS++;
+                }
+                else{
+                    skipS--;
+                }
+                i--;
+            }
+            while (j >= 0 && (t[j] == '#' || skipT > 0)){
+                if (t[j] == '#'){
+                    skipT++;
+                }
+                else{
+                    skipT--;
+                }
+                j--;
+            }
+            if (i < 0 ^ j < 0) return false;
+            if (i >= 0 && j >= 0 && s[i] != t[j]){
+                return false;
+            }
+            i--;
+            j--;
+        }
+        return true;
+        // travese left
+        // if a # encountered or the skip is > 0
+            // move left
     }
 };
+
+// skipS: 1
+// skipT: 0
+// Input: s = "ac", t = "ac"
