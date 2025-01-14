fc6c61db0bcbf1340d46141e66ea78498291469a
AhmedZ70 <101619413+AhmedZ70@users.noreply.github.com>
2025-01-13 20:31:53 -0700

    Time: 1 ms (35.19%) | Memory: 9.5 MB (31.34%) - LeetSync

--- 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.cpp
+++ 166-fraction-to-recurring-decimal/fraction-to-recurring-decimal.cpp
@@ -1,58 +1,32 @@
 class Solution {
-
-using ll = long long;
-
 public:
+    using ll = long long; 
     string fractionToDecimal(int numerator, int denominator) {
-        // handle 0 numerator
+        // if num = 0 return 0 
         if (numerator == 0) return "0"; 
-        // handle sign
         string result; 
         if (numerator < 0 ^ denominator < 0){
             result += "-";
         }
-        // convert to pos values to avoid over flow
         ll num = abs((ll)numerator); 
-        ll den = abs((ll)denominator);
-        // add integer part to result string 
-        result += to_string(num/den);
-        // store remainder if it's zero return result
-        ll remainder = num % den;
-        unordered_map<ll, int> remMap; 
+        ll den = abs((ll)denominator); 
+        
+        result += to_string(num / den); 
+        ll remainder = num % den; 
         if (remainder == 0) return result;
-        result += ".";
-
+        result += "."; 
+        unordered_map<ll, int> remMap; 
         while (remainder != 0){
-            // found same remainder
             if (remMap.find(remainder) != remMap.end()){
-                result.insert(remMap[remainder], "(");
+                result.insert(remMap[remainder], "("); 
                 result += ")";
-                break;
+                break; 
             }
             remMap[remainder] = result.size(); 
-            remainder *= 10;
-            result += to_string(remainder / den);
-            remainder %= den;
+            remainder *= 10; 
+            result += to_string(remainder / den); 
+            remainder %= den; 
         }
         return result;
-        // else add "." to result 
-        // long division
-        // if same remainder again then insert "(" at after the "."
     }
 };
-
-// 4 / 333
-//          0.(012)012012 (.012) repeating 
-//        ------ r = 67
-//     333| 4.00000
-//        | 0   
-//          ---
-//          400
-//          333
-//          ---
-//          670
-//          666
-//          ---
-//          400
-
-// map: 
