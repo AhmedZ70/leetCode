5534715c09698d58d36cf7fa080d25c145bb2ef1
AhmedZ70 <101619413+AhmedZ70@users.noreply.github.com>
2025-01-15 20:19:31 -0700

    Time: 66 ms (12.71%) | Memory: 34.3 MB (10.02%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,24 +1,32 @@
+
+
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        // ["eat","tea","tan","ate","nat","bat"]
-        // assuming that we have a map with words that have sorted letters
-        // #1 iterate over each word
-        // #2 sort it 
-        // #3 if it doesn't exist in the map
-                // we add new list
-        // #4 add that work at the index
-        vector<vector<string>> result; 
-        unordered_map<string, int> index; 
-        for (string s : strs){
-            string t = s; 
-            sort(t.begin(), t.end());
-            if (index.find(t) == index.end()){
-                index[t] = result.size();
-                result.push_back({});
+        unordered_map<string, vector<string>> hashMap;
+
+        for (const string& s : strs) {
+            // Frequency count array (size 26 for a-z)
+            vector<int> freq(26, 0);
+            for (char c : s) {
+                freq[c - 'a']++;  // Count frequency of each character
             }
-            result[index[t]].push_back(s);
+
+            // Convert frequency array into a unique string key
+            string key = "";
+            for (int count : freq) {
+                key += to_string(count) + "#";  // Use '#' as separator
+            }
+
+            hashMap[key].push_back(s);
         }
+
+        // Extract grouped anagrams into the result
+        vector<vector<string>> result;
+        for (auto& [key, group] : hashMap) {
+            result.push_back(group);
+        }
+
         return result;
     }
 };
