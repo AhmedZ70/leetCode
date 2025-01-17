70e81dc19f4a15f07505991beee5e6d120b80cf5
AhmedZ70 <101619413+AhmedZ70@users.noreply.github.com>
2025-01-17 12:36:15 -0700

    Time: 0 ms (100.00%) | Memory: 9.3 MB (63.53%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,36 +1,36 @@
 class Solution {
-private: 
-    int num = 1;
-    // Directions: right, down, left, up
-    vector<pair<int, int>> directions = {
-        {0, 1}, {1, 0}, {0, -1}, {-1, 0}
-    };
-
-    void traverseMatrix(vector<vector<int>>& matrix, int x, int y, int dir, int n) {
-        // Stop when all numbers are filled
-        if (num > n * n) return;
-
-        // Fill the current cell
-        matrix[x][y] = num++;
-
-        int nx = x + directions[dir].first;
-        int ny = y + directions[dir].second;
-
-        // Check if the next cell is out of bounds or already filled
-        if (nx < 0 || nx >= n || ny < 0 || ny >= n || matrix[nx][ny] != 0) {
-            dir = (dir + 1) % 4;
-            nx = x + directions[dir].first;
-            ny = y + directions[dir].second;
-        }
-
-        traverseMatrix(matrix, nx, ny, dir, n);
-    }
-
 public:
     vector<vector<int>> generateMatrix(int n) {
         vector<vector<int>> matrix(n, vector<int>(n, 0));
-        // Start at (0, 0) moving right (dir = 0)
-        traverseMatrix(matrix, 0, 0, 0, n); 
+        int num = 1;
+        int top = 0, bottom = n - 1, left = 0, right = n - 1;
+
+        while (num <= n * n) {
+            // Traverse from left to right
+            for (int col = left; col <= right; ++col) 
+                matrix[top][col] = num++;
+            ++top;
+
+            // Traverse from top to bottom
+            for (int row = top; row <= bottom; ++row) 
+                matrix[row][right] = num++;
+            --right;
+
+            // Traverse from right to left
+            if (top <= bottom) {
+                for (int col = right; col >= left; --col) 
+                    matrix[bottom][col] = num++;
+                --bottom;
+            }
+
+            // Traverse from bottom to top
+            if (left <= right) {
+                for (int row = bottom; row >= top; --row) 
+                    matrix[row][left] = num++;
+                ++left;
+            }
+        }
+
         return matrix;
     }
 };
